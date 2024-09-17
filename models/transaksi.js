59826a0ab2c9ab06af60b4015862974ac506fe58
daradk1707 <daradk176@gmail.com>
2024-09-17 21:45:14 +0700

    database

---
+++ models/transaksi.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class transaksi extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.hasOne(models.detail_transaksi, {
+        foreignKey: "id_transaksi", as:"transaksi"
+      })
+      this.belongsTo(models.user, {foreignKey: "id_user"})
+      this.belongsTo(models.meja, {foreignKey: "id_meja"})
+    }
+  }
+  transaksi.init({
+    id_transaksi : {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    tgl_transaksi: DataTypes.DATE,
+    id_user: DataTypes.INTEGER,
+    id_meja: DataTypes.INTEGER,
+    nama_pelanggan: DataTypes.STRING,
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'transaksi',
+  });
+  return transaksi;
+};
